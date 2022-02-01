d3a503b6ae99066768ff95a00dc5275216821fb7
eddie nyambudzi <e.nyambudzi@googlemail.com>
2022-02-01 16:02:14 +0000

    adding 12n transit gateway routes

--- transit-gateway-cloud-platform/outputs.tf
+++ transit-gateway-cloud-platform/outputs.tf
@@ -1,3 +1,15 @@
 output "cloudplatform_cidr_range" {
     value = "${local.cloudplatform_cidr_range}"
 }
+
+output "i2n_az1_cidr_range" {
+    value = "${local.i2n_az1_cidr_range}"
+}
+
+output "i2n_az2_cidr_range" {
+    value = "${local.i2n_az2_cidr_range}"
+}
+
+output "i2n_az3_cidr_range" {
+    value = "${local.i2n_az3_cidr_range}"
+}
