ce915b83398103ab1232f716afea9f9fc642e4d0
adrianweetman <adrian.weetman@digital.justice.gov.uk>
2022-06-27 15:52:53 +0100

    Enable analytics platform -> pre-prod DB AZ2 route

--- transit-gateway-analytics-platform/routes-analytical-platform-pre-prod.tf
+++ transit-gateway-analytics-platform/routes-analytical-platform-pre-prod.tf
@@ -51,12 +51,12 @@
 //   count                  = "${local.env_create_analytics_prod_routes}"
 // }
 
-// resource "aws_route" "transit_gateway_route_vpc_db-routetable-az2-apde-prod" {
-//   route_table_id         = "${data.terraform_remote_state.vpc.vpc_db-routetable-az2}"
-//   transit_gateway_id     = "${data.terraform_remote_state.common.transit_gateway_id}"
-//   destination_cidr_block = "${local.analyticalplatform_prod_cidr_range}"
-//   count                  = "${local.env_create_analytics_prod_routes}"
-// }
+resource "aws_route" "transit_gateway_route_vpc_db-routetable-az2-apde-prod" {
+   route_table_id         = "${data.terraform_remote_state.vpc.vpc_db-routetable-az2}"
+   transit_gateway_id     = "${data.terraform_remote_state.common.transit_gateway_id}"
+   destination_cidr_block = "${local.analyticalplatform_pre_prod_cidr_range}"
+   count                  = "${local.env_create_analytics_pre_prod_routes}"
+}
 
 resource "aws_route" "transit_gateway_route_vpc_db-routetable-az3-apde-pre-prod" {
   route_table_id         = "${data.terraform_remote_state.vpc.vpc_db-routetable-az3}"
