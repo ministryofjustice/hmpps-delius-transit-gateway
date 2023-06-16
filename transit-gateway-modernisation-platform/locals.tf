0875a6cbf29581b10cb5406d08db94d31ce9709c
George Taylor <george.taylor@digital.justice.gov.uk>
2023-06-16 13:36:44 +0100

    remove for_each as its unsupported

--- transit-gateway-modernisation-platform/locals.tf
+++ transit-gateway-modernisation-platform/locals.tf
@@ -10,20 +10,20 @@ locals {
 
   # Modernisation Platform
   # see https://github.com/ministryofjustice/modernisation-platform/blob/main/cidr-allocation.md
-  mp_cidr_ranges_low  = ["10.26.0.0/16"]
-  mp_cidr_ranges_high = ["10.27.0.0/16"]
+  mp_cidr_range_low  = "10.26.0.0/16"
+  mp_cidr_range_high = "10.27.0.0/16"
 
   # Only create the routes to allow connectivity testing in these environments for modernisation platform
   create_modplatform_routes_low = {
-    delius-core-dev = true
-    delius-test     = true
+    delius-core-dev = "1"
+    delius-test     = "1"
   }
-  env_create_modplatform_routes_low = "${lookup(local.create_modplatform_routes_low, "${local.environment_name}", false)}"
+  env_create_modplatform_routes_low = "${lookup(local.create_modplatform_routes_low, "${local.environment_name}", 0)}"
 
   create_modplatform_routes_high = {
-    delius-stage    = true
-    delius-pre-prod = true
-    delius-prod     = true
+    delius-stage    = "1"
+    delius-pre-prod = "1"
+    delius-prod     = "1"
   }
-  env_create_modplatform_routes_high = "${lookup(local.create_modplatform_routes_high, "${local.environment_name}", false)}"
+  env_create_modplatform_routes_high = "${lookup(local.create_modplatform_routes_high, "${local.environment_name}", 0)}"
 }
