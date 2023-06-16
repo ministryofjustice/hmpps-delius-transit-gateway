dcdd22126215a9057fa657ab8c5c9cbdc053126a
George Taylor <george.taylor@digital.justice.gov.uk>
2023-06-16 11:08:48 +0100

    low/high env separation

--- transit-gateway-modernisation-platform/locals.tf
+++ transit-gateway-modernisation-platform/locals.tf
@@ -7,15 +7,20 @@ locals {
 
 
   # Modernisation Platform
-  mp_cidr_ranges = [""]
+  mp_cidr_ranges_low  = ["10.26.0.0/16"]
+  mp_cidr_ranges_high = ["10.27.0.0/16"]
 
   # Only create the routes to allow connectivity testing in these environments for modernisation platform
-  create_modplatform_routes = {
+  create_modplatform_routes_low = {
     delius-core-dev = true
     delius-test     = true
+  }
+  env_create_modplatform_routes_low = lookup(local.create_modplatform_routes_low, "${local.environment_name}", false)
+
+  create_modplatform_routes_high = {
     delius-stage    = true
     delius-pre-prod = true
     delius-prod     = true
   }
-  env_create_modplatform_routes = lookup(local.create_modplatform_routes, "${local.environment_name}", false)
+  env_create_modplatform_routes_high = lookup(local.create_modplatform_routes_high, "${local.environment_name}", false)
 }
