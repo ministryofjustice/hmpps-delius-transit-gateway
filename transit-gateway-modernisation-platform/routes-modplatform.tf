1377b7f79b5d929b16e96d4174bc2252a4d9dc8c
George Taylor <george.taylor@digital.justice.gov.uk>
2023-06-16 13:52:02 +0100

    left in for each

--- transit-gateway-modernisation-platform/routes-modplatform.tf
+++ transit-gateway-modernisation-platform/routes-modplatform.tf
@@ -12,63 +12,63 @@ resource "aws_route" "transit_gateway_route_vpc_public-routetable-az1" {
   count                  = "${local.env_create_modplatform_routes_low}"
   route_table_id         = "${data.terraform_remote_state.vpc.vpc_public-routetable-az1}"
   transit_gateway_id     = "${data.terraform_remote_state.common.pttp_transit_gateway_id}"
-  destination_cidr_block = "${each.value}"
+  destination_cidr_block = "${local.mp_cidr_range_low}"
 }
 
 resource "aws_route" "transit_gateway_route_vpc_public-routetable-az2" {
   count                  = "${local.env_create_modplatform_routes_low}"
   route_table_id         = "${data.terraform_remote_state.vpc.vpc_public-routetable-az2}"
   transit_gateway_id     = "${data.terraform_remote_state.common.pttp_transit_gateway_id}"
-  destination_cidr_block = "${each.value}"
+  destination_cidr_block = "${local.mp_cidr_range_low}"
 }
 
 resource "aws_route" "transit_gateway_route_vpc_public-routetable-az3" {
   count                  = "${local.env_create_modplatform_routes_low}"
   route_table_id         = "${data.terraform_remote_state.vpc.vpc_public-routetable-az3}"
   transit_gateway_id     = "${data.terraform_remote_state.common.pttp_transit_gateway_id}"
-  destination_cidr_block = "${each.value}"
+  destination_cidr_block = "${local.mp_cidr_range_low}"
 }
 
 resource "aws_route" "transit_gateway_route_vpc_private-routetable-az1" {
   count                  = "${local.env_create_modplatform_routes_low}"
   route_table_id         = "${data.terraform_remote_state.vpc.vpc_private-routetable-az1}"
   transit_gateway_id     = "${data.terraform_remote_state.common.pttp_transit_gateway_id}"
-  destination_cidr_block = "${each.value}"
+  destination_cidr_block = "${local.mp_cidr_range_low}"
 }
 
 resource "aws_route" "transit_gateway_route_vpc_private-routetable-az2" {
   count                  = "${local.env_create_modplatform_routes_low}"
   route_table_id         = "${data.terraform_remote_state.vpc.vpc_private-routetable-az2}"
   transit_gateway_id     = "${data.terraform_remote_state.common.pttp_transit_gateway_id}"
-  destination_cidr_block = "${each.value}"
+  destination_cidr_block = "${local.mp_cidr_range_low}"
 }
 
 resource "aws_route" "transit_gateway_route_vpc_private-routetable-az3" {
   count                  = "${local.env_create_modplatform_routes_low}"
   route_table_id         = "${data.terraform_remote_state.vpc.vpc_private-routetable-az3}"
   transit_gateway_id     = "${data.terraform_remote_state.common.pttp_transit_gateway_id}"
-  destination_cidr_block = "${each.value}"
+  destination_cidr_block = "${local.mp_cidr_range_low}"
 }
 
 resource "aws_route" "transit_gateway_route_vpc_db-routetable-az1" {
   count                  = "${local.env_create_modplatform_routes_low}"
   route_table_id         = "${data.terraform_remote_state.vpc.vpc_db-routetable-az1}"
   transit_gateway_id     = "${data.terraform_remote_state.common.pttp_transit_gateway_id}"
-  destination_cidr_block = "${each.value}"
+  destination_cidr_block = "${local.mp_cidr_range_low}"
 }
 
 resource "aws_route" "transit_gateway_route_vpc_db-routetable-az2" {
   count                  = "${local.env_create_modplatform_routes_low}"
   route_table_id         = "${data.terraform_remote_state.vpc.vpc_db-routetable-az2}"
   transit_gateway_id     = "${data.terraform_remote_state.common.pttp_transit_gateway_id}"
-  destination_cidr_block = "${each.value}"
+  destination_cidr_block = "${local.mp_cidr_range_low}"
 }
 
 resource "aws_route" "transit_gateway_route_vpc_db-routetable-az3" {
   count                  = "${local.env_create_modplatform_routes_low}"
   route_table_id         = "${data.terraform_remote_state.vpc.vpc_db-routetable-az3}"
   transit_gateway_id     = "${data.terraform_remote_state.common.pttp_transit_gateway_id}"
-  destination_cidr_block = "${each.value}"
+  destination_cidr_block = "${local.mp_cidr_range_low}"
 }
 
 #######################
@@ -79,61 +79,61 @@ resource "aws_route" "transit_gateway_route_vpc_public-routetable-az1" {
   count                  = "${local.env_create_modplatform_routes_high}"
   route_table_id         = "${data.terraform_remote_state.vpc.vpc_public-routetable-az1}"
   transit_gateway_id     = "${data.terraform_remote_state.common.pttp_transit_gateway_id}"
-  destination_cidr_block = "${each.value}"
+  destination_cidr_block = "${local.mp_cidr_range_high}"
 }
 
 resource "aws_route" "transit_gateway_route_vpc_public-routetable-az2" {
   count               = "${local.env_create_modplatform_routes_high}"
   route_table_id         = "${data.terraform_remote_state.vpc.vpc_public-routetable-az2}"
   transit_gateway_id     = "${data.terraform_remote_state.common.pttp_transit_gateway_id}"
-  destination_cidr_block = "${each.value}"
+  destination_cidr_block = "${local.mp_cidr_range_high}"
 }
 
 resource "aws_route" "transit_gateway_route_vpc_public-routetable-az3" {
   count               = "${local.env_create_modplatform_routes_high}"
   route_table_id         = "${data.terraform_remote_state.vpc.vpc_public-routetable-az3}"
   transit_gateway_id     = "${data.terraform_remote_state.common.pttp_transit_gateway_id}"
-  destination_cidr_block = "${each.value}"
+  destination_cidr_block = "${local.mp_cidr_range_high}"
 }
 
 resource "aws_route" "transit_gateway_route_vpc_private-routetable-az1" {
   count               = "${local.env_create_modplatform_routes_high}"
   route_table_id         = "${data.terraform_remote_state.vpc.vpc_private-routetable-az1}"
   transit_gateway_id     = "${data.terraform_remote_state.common.pttp_transit_gateway_id}"
-  destination_cidr_block = "${each.value}"
+  destination_cidr_block = "${local.mp_cidr_range_high}"
 }
 
 resource "aws_route" "transit_gateway_route_vpc_private-routetable-az2" {
   count               = "${local.env_create_modplatform_routes_high}"
   route_table_id         = "${data.terraform_remote_state.vpc.vpc_private-routetable-az2}"
   transit_gateway_id     = "${data.terraform_remote_state.common.pttp_transit_gateway_id}"
-  destination_cidr_block = "${each.value}"
+  destination_cidr_block = "${local.mp_cidr_range_high}"
 }
 
 resource "aws_route" "transit_gateway_route_vpc_private-routetable-az3" {
   count               = "${local.env_create_modplatform_routes_high}"
   route_table_id         = "${data.terraform_remote_state.vpc.vpc_private-routetable-az3}"
   transit_gateway_id     = "${data.terraform_remote_state.common.pttp_transit_gateway_id}"
-  destination_cidr_block = "${each.value}"
+  destination_cidr_block = "${local.mp_cidr_range_high}"
 }
 
 resource "aws_route" "transit_gateway_route_vpc_db-routetable-az1" {
   count               = "${local.env_create_modplatform_routes_high}"
   route_table_id         = "${data.terraform_remote_state.vpc.vpc_db-routetable-az1}"
   transit_gateway_id     = "${data.terraform_remote_state.common.pttp_transit_gateway_id}"
-  destination_cidr_block = "${each.value}"
+  destination_cidr_block = "${local.mp_cidr_range_high}"
 }
 
 resource "aws_route" "transit_gateway_route_vpc_db-routetable-az2" {
   count               = "${local.env_create_modplatform_routes_high}"
   route_table_id         = "${data.terraform_remote_state.vpc.vpc_db-routetable-az2}"
   transit_gateway_id     = "${data.terraform_remote_state.common.pttp_transit_gateway_id}"
-  destination_cidr_block = "${each.value}"
+  destination_cidr_block = "${local.mp_cidr_range_high}"
 }
 
 resource "aws_route" "transit_gateway_route_vpc_db-routetable-az3" {
   count               = "${local.env_create_modplatform_routes_high}"
   route_table_id         = "${data.terraform_remote_state.vpc.vpc_db-routetable-az3}"
   transit_gateway_id     = "${data.terraform_remote_state.common.pttp_transit_gateway_id}"
-  destination_cidr_block = "${each.value}"
+  destination_cidr_block = "${local.mp_cidr_range_high}"
 }
