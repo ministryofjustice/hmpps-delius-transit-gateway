2a6e8f5c86096679b81c8a87920ba562d14cf967
George Taylor <george.taylor@digital.justice.gov.uk>
2023-11-07 09:33:05 +0000

    Update routes-analytical-platform-prod.tf

--- transit-gateway-analytics-platform/routes-analytical-platform-prod.tf
+++ transit-gateway-analytics-platform/routes-analytical-platform-prod.tf
@@ -44,19 +44,19 @@
 //   count                  = "${local.env_create_analytics_prod_routes}"
 // }
 
-// resource "aws_route" "transit_gateway_route_vpc_db-routetable-az1-apde-prod" {
-//   route_table_id         = "${data.terraform_remote_state.vpc.vpc_db-routetable-az1}"
-//   transit_gateway_id     = "${data.terraform_remote_state.common.pttp_transit_gateway_id}"
-//   destination_cidr_block = "${local.analyticalplatform_prod_cidr_range}"
-//   count                  = "${local.env_create_analytics_prod_routes}"
-// }
+resource "aws_route" "transit_gateway_route_vpc_db-routetable-az1-apde-prod" {
+  route_table_id         = "${data.terraform_remote_state.vpc.vpc_db-routetable-az1}"
+  transit_gateway_id     = "${data.terraform_remote_state.common.pttp_transit_gateway_id}"
+  destination_cidr_block = "${local.analyticalplatform_prod_cidr_range}"
+  count                  = "${local.env_create_analytics_prod_routes}"
+}
 
-// resource "aws_route" "transit_gateway_route_vpc_db-routetable-az2-apde-prod" {
-//   route_table_id         = "${data.terraform_remote_state.vpc.vpc_db-routetable-az2}"
-//   transit_gateway_id     = "${data.terraform_remote_state.common.pttp_transit_gateway_id}"
-//   destination_cidr_block = "${local.analyticalplatform_prod_cidr_range}"
-//   count                  = "${local.env_create_analytics_prod_routes}"
-// }
+resource "aws_route" "transit_gateway_route_vpc_db-routetable-az2-apde-prod" {
+  route_table_id         = "${data.terraform_remote_state.vpc.vpc_db-routetable-az2}"
+  transit_gateway_id     = "${data.terraform_remote_state.common.pttp_transit_gateway_id}"
+  destination_cidr_block = "${local.analyticalplatform_prod_cidr_range}"
+  count                  = "${local.env_create_analytics_prod_routes}"
+}
 
 resource "aws_route" "transit_gateway_route_vpc_db-routetable-az3-apde-prod" {
   route_table_id         = "${data.terraform_remote_state.vpc.vpc_db-routetable-az3}"
